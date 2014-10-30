d655e2fb6398220ecf7e8ebec56503e842502b7d
Bilge Acun <acun2@illinois.edu>
2014-10-29 21:50:57 -0500

    remove everything
    Change-Id: I07919f1568512d1473ab51cbcd96d0d6d5d1c19c

--- tests/modelnet-p2p-bw-loggp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-tests/modelnet-p2p-bw --sync=1 -- tests/conf/modelnet-p2p-bw-loggp.conf
--- tests/modelnet-test-dragonfly.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-tests/modelnet-test --sync=1 -- tests/conf/modelnet-test-dragonfly.conf
