13c54cafa0c038798811c1828222bf9e522c526c
Bilge Acun <acun2@illinois.edu>
2014-10-29 21:50:57 -0500

    remove everything
    Change-Id: I07919f1568512d1473ab51cbcd96d0d6d5d1c19c

--- tests/bigsim/entities/MsgEntry.d
+++
@@ -1,4 +0,0 @@
-entities/MsgEntry.d entities/MsgEntry.o: entities/MsgEntry.cpp \
- entities/MsgEntry.h
-
-entities/MsgEntry.h:
--- tests/bigsim/entities/PE.d
+++
@@ -1,8 +0,0 @@
-entities/PE.d entities/PE.o: entities/PE.cpp entities/PE.h \
- entities/MsgEntry.h entities/Task.h
-
-entities/PE.h:
-
-entities/MsgEntry.h:
-
-entities/Task.h:
