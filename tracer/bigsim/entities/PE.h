5e66ad7484ee4eab039564ab1d3aa74b8ea815c3
Nikhil Jain <nikhil.jain@acm.org>
2015-03-23 15:10:15 -0500

    Add tracer folder
    Change-Id: I2cb57612afeb222bbb1e81854d4bdd5658ffa041

---
+++ tracer/bigsim/entities/PE.h
@@ -0,0 +1,53 @@
+/*
+ * PE.h
+ *
+ *  Created on: Oct 20, 2010
+ *      Author: ehsan
+ */
+
+#ifndef PE_H_
+#define PE_H_
+
+#include "MsgEntry.h"
+#include <cstring>
+#include "Task.h"
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <map>
+
+using namespace std;
+class Task;
+
+class PE {
+  public:
+    PE();
+    ~PE();
+    list<int> msgBuffer;
+    vector<bool> busyStateBuffer;
+    map<int, vector<int> > taskMsgBuffer; //For optimistic mode: store copy of the messages received per task
+    Task* myTasks;	// all tasks of this PE
+    unsigned long long currTime;
+    bool busy;
+    int windowOffset;
+    int beforeTask, totalTasksCount;
+    int myNum, myEmPE;
+    int tasksCount;	//total number of tasks
+    int currentTask; // index of first not-executed task (helps searching messages)
+
+    bool noUnsatDep(int tInd);	// there is no unsatisfied dependency for task
+    unsigned long long taskExecTime(int tInd);
+    void printStat();
+    void check();
+    void printState();
+
+    //functions added by Bilge for codes-tracing
+    void invertMsgPe(int tInd);
+    unsigned long long getTaskExecTime(int tInd);
+    map<int, int>* msgDestLogs;
+    int findTaskFromMsg(MsgID* msg);
+    int numWth, numEmPes;
+
+};
+
+#endif /* PE_H_ */
