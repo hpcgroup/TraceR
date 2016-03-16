a7002024fd2e29b924b59240894488c0d3597d9e
Nikhil Jain <nikhil.jain@acm.org>
2016-03-16 11:26:00 -0700

    More mappings

---
+++ utils/stencil_block_node_random_mapping.C
@@ -0,0 +1,142 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2015, Lawrence Livermore National Security, LLC.
+// Produced at the Lawrence Livermore National Laboratory.
+//
+// Written by:
+//     Nikhil Jain <nikhil.jain@acm.org>
+//     Bilge Acun <acun2@illinois.edu>
+//     Abhinav Bhatele <bhatele@llnl.gov>
+//
+// LLNL-CODE-681378. All rights reserved.
+//
+// This file is part of TraceR. For details, see:
+// https://github.com/LLNL/tracer
+// Please also read the LICENSE file for our notice and the LGPL.
+//////////////////////////////////////////////////////////////////////////////
+
+#include <cstdio>
+#include <cstdlib>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char**argv) {
+  FILE *binout = fopen(argv[1], "wb");
+  int jobid = 0;
+  FILE* out_files;
+
+  if(argc < 8) {
+    printf("Correct usage: %s <global_file_name> <total ranks> <number of nodes per router> <number of ranks per node> <skip after node> <number of groups> <number of routers per group> <stencil dims> <block dims>\n",
+        argv[0]);
+    exit(1);
+  }
+
+  char dFILE[256];
+  sprintf(dFILE, "%s%d", "job", 0);
+  out_files = fopen(dFILE, "wb");
+
+  int numAllocCores = atoi(argv[2]);
+  int rr_group = atoi(argv[3]);
+  int rr = atoi(argv[4]);
+  int skip = atoi(argv[5]);
+  int numGroups = atoi(argv[6]);
+  int numRouters = atoi(argv[7]);
+  int local_rank = 0;
+  int box_x = atoi(argv[8]);
+  int box_y = atoi(argv[9]);
+  int box_z = atoi(argv[10]);
+  int s_x = atoi(argv[11]);
+  int s_y = atoi(argv[12]);
+  int s_z = atoi(argv[13]);
+
+  int *localRanks = new int[numAllocCores];
+
+  int size_per_group = numRouters * rr_group * rr;
+
+  int numNodes = numGroups * numRouters * skip;
+  int *mapping = new int[numNodes];
+  int *granks = new int[numAllocCores];
+  
+  int bx = box_x/s_x;
+  int by = box_y/s_y;
+  int bz = box_z/s_z;
+  int prod_xyz = s_x * s_y * s_z;
+  int prod_xy = s_x * s_y;
+
+  for(int i = 0; i < numAllocCores; i++) {
+    int box_num = i / prod_xyz;
+    int box_rank = i % prod_xyz;
+    int x = box_num % bx;
+    int y = (box_num / bx) % by;
+    int z = box_num / (bx*by);
+
+    int my_x = x * s_x + box_rank % s_x;
+    int my_y = y * s_y + (box_rank / s_x) % s_y;
+    int my_z = z * s_z + box_rank / (s_x * s_y);
+
+    int my_rank = my_x + my_y * box_x + my_z * box_x * box_y;
+   
+    localRanks[i] = my_rank;
+  }
+
+  for(int i = 0; i < numNodes; i++) {
+    mapping[i] = i;
+  }
+
+  srand(1331);
+  for(int i = 0; i < numNodes; i++) {
+    int node1 = rand() % numNodes;
+    int node2 = rand() % numNodes;
+    int temp = mapping[node1];
+    mapping[node1] = mapping[node2];
+    mapping[node2] = temp;
+  }
+
+  int currNode = 0;
+  for(int g = 0; g < numGroups; g++) {
+    for(int r = 0; r < numRouters; r++) {
+      for(int j = 0; j < rr_group; j++) {
+        if(j >= skip) {
+          break;
+        }
+        int i = g * size_per_group + r * rr_group * rr;
+        int useRank = mapping[currNode] * rr;
+        if(useRank >= numAllocCores) {
+          currNode++;
+          continue;
+        }
+        for(int k = 0; k < rr; k++) {
+          int global_rank = i + j + k * rr_group;
+          fwrite(&global_rank, sizeof(int), 1, binout);
+          fwrite(&localRanks[useRank], sizeof(int), 1, binout);
+          fwrite(&jobid, sizeof(int), 1, binout);
+          granks[localRanks[useRank]] = global_rank;
+#if PRINT_MAP
+          printf("%d %d %d\n", global_rank, localRanks[useRank++], jobid);
+#else
+          useRank++;
+#endif
+          local_rank++;
+          if(local_rank == numAllocCores) {
+            break;
+          }
+        }
+        if(local_rank == numAllocCores) {
+          break;
+        }
+        currNode++;
+      }
+      if(local_rank == numAllocCores) {
+        break;
+      }
+    }
+  }
+  
+  for(int i = 0; i < numAllocCores; i++) {
+    fwrite(&granks[i], sizeof(int), 1, out_files);
+  }
+
+  fclose(binout);
+  fclose(out_files);
+}
