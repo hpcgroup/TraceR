7d7e59f23b9a3b97597ed1e169a31c354b90ada9
Ryan Mast <mast9@llnl.gov>
2019-06-28 14:10:13 -0700

    Add WallTime macro for loop body timing in code example

--- docs/code-examples/scorep_user_calls.c
+++ docs/code-examples/scorep_user_calls.c
@@ -6,20 +6,28 @@ int main(int argc, char **argv, char **envp)
   SCOREP_RECORDING_OFF(); //turn recording off for initialization/regions not of interest
   ...
   SCOREP_RECORDING_ON();
+
   //use verbatim to facilitate looping over the traces in simulation when simulating multiple jobs
   SCOREP_USER_REGION_BY_NAME_BEGIN("TRACER_Loop", SCOREP_USER_REGION_TYPE_COMMON); 
   // at least add this BEGIN timer call - called from only one rank
   // you can add more calls later with region names TRACER_WallTime_<any string of your choice>
+
   if(myRank == 0)
-    SCOREP_USER_REGION_BY_NAME_BEGIN("TRACER_WallTime_MainLoop", SCOREP_USER_REGION_TYPE_COMMON);
+    SCOREP_USER_REGION_BY_NAME_BEGIN("TRACER_WallTime_Loop", SCOREP_USER_REGION_TYPE_COMMON);
+
   // Application main work LOOP
   for ( int itscf = 0; itscf < nitscf_; itscf++ )
   {
+    // time call to mark start of loop iteration
+    SCOREP_USER_REGION_BY_NAME_BEGIN("TRACER_WallTime_Loop_Iter", SCOREP_USER_REGION_TYPE_COMMON);
     ...
+    SCOREP_USER_REGION_BY_NAME_END("TRACER_WallTime_Loop_Iter");
   }
+
   // time call to mark END of work - called from only one rank
   if(myRank == 0)
-    SCOREP_USER_REGION_BY_NAME_END("TRACER_WallTime_MainLoop");
+    SCOREP_USER_REGION_BY_NAME_END("TRACER_WallTime_Loop");
+
   // use verbatim - mark end of trace loop
   SCOREP_USER_REGION_BY_NAME_END("TRACER_Loop"); 
   SCOREP_RECORDING_OFF();//turn off recording again
